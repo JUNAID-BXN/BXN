b09e518eb94fbc16c0c12433916fd05632abd726
JUNAID-BXN <115928284+JUNAID-BXN@users.noreply.github.com>
2022-10-16 16:48:25 +0500

    Update and rename BXN.py to Run.py

--- Run.py
+++ Run.py
@@ -2,7 +2,7 @@ import platform
 bit=platform.architecture()[0]
 if bit =='64bit':
     import BXN
-    BXN()
+    BXN.main()
 else:
     print('Sorry device or system not support this tools')
     exit()
